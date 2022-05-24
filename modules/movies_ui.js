a7bf5432707a1ad874a6e46091fbbfa3116d2a4c
yosefHesham <yousefmobdev@gmail.com>
2022-05-24 18:08:23 +0200

    Display rate and name

--- modules/movies_ui.js
+++ modules/movies_ui.js
@@ -11,13 +11,28 @@ export const renderTopRated = async () => {
     let i = 0;
     topRated.forEach(movie => {
       i+=1
-      const element = document.createElement("img");
+      const tmp = document.createElement("tmp");
+      tmp.innerHTML = `<article class="movie-container" data=${movie.id}">
+        <div class="movie-card">
+        <img src="${movie.image}" alt="${movie.title}">
+        <div class="movie-data">
+          <p class="title"> ${movie.title} </p>
+          <div class="rate-wrapper">
+          <span class = "fa fa-star checked"></span>  
+          <p class="rate"> ${movie.rate} </p>
+
+        </div>
+        </div>
+
+        <div class="movie-interaction">
+          <button> Comments </button>
+          <p>  like </p>
+        </div>
+      </article>`
       
-      element.src = movie.image;
-      console.log(element);
       setTimeout(() => {
-        topRatedSection.appendChild(element);
-      }, 500 * i)
+        topRatedSection.appendChild(tmp.firstChild);
+      }, 300 * i)
 
     });
 } 
