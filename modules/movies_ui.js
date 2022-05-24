1ba6e72b9c880008738fa95113d15cfb127f82c5
yosefHesham <yousefmobdev@gmail.com>
2022-05-24 22:38:08 +0200

    Refactor renderTopRated method

--- modules/movies_ui.js
+++ modules/movies_ui.js
@@ -1,38 +1,58 @@
+import { toJson } from "./json_helper";
 import Movie from "./movie";
 import MovieService from "./movie_service";
 
 export const topRatedSection = document.querySelector(".top-rated");
+const popularSection = document.querySelector(".popular");
 
+/**
+ * 
+ * @param {Movie} movie 
+ * @returns 
+ */
+const createMovieCard = (movie) => {
+  const tmp = document.createElement("tmp");
+  tmp.innerHTML = `<article class="movie-container" data=${toJson(movie)}">
+    <div class="movie-card">
+    <img src="${movie.image}" alt="${movie.title}">
+    <div class="movie-data">
+      <p class="title"> ${movie.title} </p>
+      <div class="rate-wrapper">
+      <span class = "fa fa-star checked"></span>  
+      <p class="rate"> ${movie.rate} </p>
+
+    </div>
+    </div>
+
+    <div class="movie-interaction">
+      <button> Comments </button>
+      <p class="like">  like </p>
+    </div>
+  </article>`;
+  return tmp;
+}
+/**
+ * 
+ * @param {Array<Movie>} movies 
+ * @param {HTMLElement} section 
+ */
+const moviesIterator = (movies, section)=> {
+
+     /** @type {Array<Movie>} */ 
+     let i = 0;
+     movies.forEach(movie => {
+       i+=1
+      const temp = createMovieCard(movie);
+     
+       setTimeout(() => {
+         section.appendChild(temp.firstChild);
+       }, 300 * i)
+ 
+     });
+}
 export const renderTopRated = async () => {
     await MovieService.getTopRated();
-  
-     /** @type {Array<Movie>} */ 
     const topRated = MovieService.topRatedMovies;
-    let i = 0;
-    topRated.forEach(movie => {
-      i+=1
-      const tmp = document.createElement("tmp");
-      tmp.innerHTML = `<article class="movie-container" data=${movie.id}">
-        <div class="movie-card">
-        <img src="${movie.image}" alt="${movie.title}">
-        <div class="movie-data">
-          <p class="title"> ${movie.title} </p>
-          <div class="rate-wrapper">
-          <span class = "fa fa-star checked"></span>  
-          <p class="rate"> ${movie.rate} </p>
-
-        </div>
-        </div>
-
-        <div class="movie-interaction">
-          <button> Comments </button>
-          <p>  like </p>
-        </div>
-      </article>`
-      
-      setTimeout(() => {
-        topRatedSection.appendChild(tmp.firstChild);
-      }, 300 * i)
-
-    });
+  moviesIterator(topRated,topRatedSection);
+  
 } 
