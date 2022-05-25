5feb00400b35af3053e93f16a0469caad5835680
bizip <bizip04@gmail.com>
2022-05-25 12:44:38 +0200

    Add a popup skeleton

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ import './style.css';
 
 window.load = renderTopRated();
 window.load = renderMostPopular();
-const comment = document.querySelectorAll('.comment');
-document.addEventListener('click', (e) => {
-    console.log(e.target.dataset)
-})
