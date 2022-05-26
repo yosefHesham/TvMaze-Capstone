eb11b8ecdafbfb7a452c0d8b44d997f05dfbb72a
yosefHesham <yousefmobdev@gmail.com>
2022-05-26 11:51:52 +0200

    Fix js linter errors

--- tests/counters.test.js
+++ tests/counters.test.js
@@ -2,36 +2,33 @@
  * @jest-environment jsdom
  */
 
-import Movie from "../modules/movie";
-import MovieService from "../modules/movie_service";
+import Movie from '../modules/movie';
+import MovieService from '../modules/movie_service';
 
-it("Top rated Should return count of 2", () => {
+it('Top rated Should return count of 2', () => {
   // arrrange
- const movie =  new Movie(1,"title",8,"image","overview","date")
+  const movie = new Movie(1, 'title', 8, 'image', 'overview', 'date');
 
-   // act
-  for(let i = 0; i < 2; i +=1) {
-    MovieService.topRatedMovies.push(movie)
+  // act
+  for (let i = 0; i < 2; i += 1) {
+    MovieService.topRatedMovies.push(movie);
   }
 
-  //assert
+  // assert
 
   expect(MovieService.countTopRated()).toBe(2);
+});
 
-})
-
-
-it("Most Popular rated Should return count of 2", () => {
+it('Most Popular rated Should return count of 2', () => {
   // arrrange
- const movie =  new Movie(1,"title",8,"image","overview","date")
+  const movie = new Movie(1, 'title', 8, 'image', 'overview', 'date');
 
-   // act
-  for(let i = 0; i < 2; i +=1) {
-    MovieService.popularMovies.push(movie)
+  // act
+  for (let i = 0; i < 2; i += 1) {
+    MovieService.popularMovies.push(movie);
   }
 
-  //assert
+  // assert
 
   expect(MovieService.countMostPopular()).toBe(2);
-
-})
+});
