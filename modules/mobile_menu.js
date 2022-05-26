111531e3a7a3ec7f6da3f3e08881a0ba303b0918
yosefHesham <yousefmobdev@gmail.com>
2022-05-26 18:03:33 +0200

    Fix linter errors

--- modules/mobile_menu.js
+++ modules/mobile_menu.js
@@ -1,17 +1,15 @@
-const menuBtn = document.querySelector('.fa-bars') 
-const menu = document.querySelector('.mobile-menu')
+const menuBtn = document.querySelector('.fa-bars');
+const menu = document.querySelector('.mobile-menu');
 
-const closeBtn = document.querySelector(".fa-xmark")
+const closeBtn = document.querySelector('.fa-xmark');
 
-closeBtn.addEventListener("click", () => {
-  menu.style.display = "none";
-})
+closeBtn.addEventListener('click', () => {
+  menu.style.display = 'none';
+});
 
-const addMenuListener = () => { menuBtn.addEventListener("click",() => {
-  menu.style.display = "block";
-
-})
-}
-addMenuListener()
-
-
+const addMenuListener = () => {
+  menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+  });
+};
+addMenuListener();
